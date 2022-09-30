5df2e2c5f4acaee636ff1b6d94078c0494fefcf4
sara-hz <80097077+sara-hz@users.noreply.github.com>
2022-09-30 17:46:21 +0200

    Update and rename Fix error subs_intensities to Fix error subs_intensities.groovy

--- Fix error subs_intensities.groovy
+++ Fix error subs_intensities.groovy
@@ -1,5 +1,7 @@
 /// Script by Peter Bankhead
 
+/// When running intensity features on tiles misses some tiles from analysis. Run and redo intensity features
+
 def toRemove = getAllObjects().findAll {it.hasROI() && (it.getROI().getBoundsWidth() <= 0 || it.getROI().getBoundsHeight() <= 0)}
 
 removeObjects(toRemove, true)
